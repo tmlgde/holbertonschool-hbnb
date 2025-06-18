e8b9d1d9fdf83cd5bd57282161a7668c57ad4e54
tmlgde <tom.lagarde33@outlook.fr>
2025-06-18 11:24:17 +0000

    add get put

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+
 
 class HBnBFacade:
+
     def __init__(self):
         self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
@@ -9,10 +13,28 @@ class HBnBFacade:
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
-
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+    
+    def get_all_users(self):
+        return self.user_repo.get_all()
+    
+    def updated_ser(self, user_id, data):
+        user = self.user_repo.get(user_id)
+        if user is None:
+            return None
+        for key, value in data.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+        return user
